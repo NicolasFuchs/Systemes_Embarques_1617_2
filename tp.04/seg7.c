e2d8c86b41be12e2425779e95d3b2cfaf1ccab48
nicolas.fuchs <nicolas.fuchs@edu.hefr.ch>
2016-11-11 16:36:19 +0100

    second commit

--- tp.04/seg7.c
+++ tp.04/seg7.c
@@ -5,18 +5,11 @@
  *      Author: lmi
  */
 #include "am335x_gpio.h"
+#include <stdbool.h>
+#include <stdint.h>
 
 #define SEG_GPIO AM335X_GPIO0
 #define DIG_GPIO AM335X_GPIO2
-#define DIG1_PIN 2
-#define DIG2_PIN 3
-#define A_PIN 4
-#define B_PIN 5
-#define C_PIN 14
-#define D_PIN 22
-#define E_PIN 23
-#define F_PIN 26
-#define G_PIN 27
 
 struct gpio_init {
 	enum am335x_gpio_modules module;
@@ -25,8 +18,8 @@ struct gpio_init {
 };
 
 static const struct gpio_init gpio_init[] = {
-		{DIG_GPIO, 2, false},	//DIG1
-		{DIG_GPIO, 3, false},	//DIG2
+		{DIG_GPIO, 2, true},	//DIG1
+		{DIG_GPIO, 3, true},	//DIG2
 
 		{DIG_GPIO, 4, false},	//DP1
 		{DIG_GPIO, 5, false},	//DP2
@@ -40,12 +33,27 @@ static const struct gpio_init gpio_init[] = {
 		{SEG_GPIO, 27, false},	//SEGG
 };
 
+bool display[] = {
+		{true,true,true,true,true,true,false},
+		{false,true,true,false,false,false,false},
+		{true,true,false,true,true,false,true},
+		{true,true,true,true,false,false,true},
+		{false,true,true,false,false,false,true},
+		{true,false,true,true,false,true,false},
+		{true,false,true,true,true,true,true},
+		{true,true,true,false,false,false,false},
+		{true,true,true,true,true,true,true},
+		{true,true,true,true,false,false,true}
+};
+
 void seg7_init() {
 	am335x_gpio_init(SEG_GPIO);
 	am335x_gpio_init(DIG_GPIO);
-	am335x_gpio_setup_pin_out(CHA_GPIO,CHA_PIN,AM335X_GPIO_PIN_IN,AM335X_GPIO_PULL_NONE);
-	am335x_gpio_setup_pin_out(CHB_GPIO,CHB_PIN,AM335X_GPIO_PIN_IN,AM335X_GPIO_PULL_NONE);
-	am335x_gpio_setup_pin_out(SW_GPIO,SW_PIN,AM335X_GPIO_PIN_IN,AM335X_GPIO_PULL_NONE);
+	int t = sizeof(gpio_init/sizeof(gpio_init[0]));
+	for (int i = 0; i < t; i++) {
+		am335x_gpio_setup_pin_out(gpio_init[i].module, gpio_init[i].pin_nr, gpio_init[i].state);
+	}
+
 }
 
 void seg7_display(int number) {
